f7fbd4f31ef1d2d3652c04fb398152e79a1ce244
Alex Robenko <arobenko@gmail.com>
2022-07-29 16:03:21 +1000

    Using embedded schema of cc_asn1.

--- app/cc_x509_dump/src/X509DefaultOptions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "cc_asn1/options/DefaultOptions.h"
-#include "cc_x509/options/DefaultOptions.h"
-
-using X509DefaultOptions = cc_x509::options::DefaultOptionsT<cc_asn1::options::DefaultOptions>;
