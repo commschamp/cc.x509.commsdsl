f56b06caf40bf399ba6917f8d818f9b1d531056a
Alex Robenko <arobenko@gmail.com>
2022-07-30 17:36:42 +1000

    Updating github actions configuration.

---
+++ script/env_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$( dirname ${SCRIPT_DIR} )
+BUILD_DIR="${ROOT_DIR}/build.test"
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+cmake .. -DCMAKE_INSTALL_PREFIX=install \
+    -DCMAKE_BUILD_TYPE=Debug -DCC_X509_BUILD_APPS=ON  "$@"
